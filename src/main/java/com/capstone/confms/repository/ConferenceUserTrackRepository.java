f6279d815edd7670fdc31776ccefc3c3d7c36f5e
gordon0201 <gordondang0201@gmail.com>
2026-03-01 21:31:31 +0700

    add getConferenceByUser-andgetUserByConference api

--- src/main/java/com/capstone/confms/repository/ConferenceUserTrackRepository.java
+++ src/main/java/com/capstone/confms/repository/ConferenceUserTrackRepository.java
@@ -1,7 +1,13 @@
 package com.capstone.confms.repository;
 
 import com.capstone.confms.entity.ConferenceUserTrack;
+import com.capstone.confms.utils.enums.ConferenceTrackRole;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConferenceUserTrackRepository extends JpaRepository<ConferenceUserTrack, Integer> {
+
+    List<ConferenceUserTrack> findByConference_IdAndAssignedRole(Integer conferenceId, ConferenceTrackRole assignedRole);
+
+    List<ConferenceUserTrack> findByUser_IdAndAssignedRole(Integer userId, ConferenceTrackRole assignedRole);
 }
